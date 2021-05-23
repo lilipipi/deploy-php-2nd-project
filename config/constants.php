cd282e5311b02062c50f44e6d034a9e120443ff9
Hung <s3715272@rmit.edu.vn>
2021-05-24 00:56:37 +1000

    fixed constants

--- config/constants.php
+++ config/constants.php
@@ -5,9 +5,9 @@ session_start();
 
 define('SITEURL', 'https://gentle-shore-25441.herokuapp.com/food-order/');
 define('LOCALHOST', 'us-cdbr-east-03.cleardb.com');
-define('DB_USERNAME', 'bdecbb76cd17fa');
-define('DB_PASSWORD', 'fce12789');
-define('DB_NAME', 'food-order');
+define('DB_USERNAME', 'b7e42ad3a7a562');
+define('DB_PASSWORD', 'b4bfc92a');
+define('DB_NAME', 'heroku_16e85c529f8feeb');
 
 
 $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error());
