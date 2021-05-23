e128595ff5b09d9e6bdbd08cb0b3666b4adebd06
Hung <s3715272@rmit.edu.vn>
2021-05-24 01:08:45 +1000

    fixed constants in food-order

--- food-order/config/constants.php
+++ food-order/config/constants.php
@@ -3,21 +3,12 @@
 // Star Session
 session_start();
 
-define('SITEURL','http://localhost/food-order/');
-define('LOCALHOST','localhost');
-define('DB_USERNAME','root');
-define('DB_PASSWORD','');
-define('DB_NAME','food-order');
+define('SITEURL', 'https://gentle-shore-25441.herokuapp.com/food-order/');
+define('LOCALHOST', 'us-cdbr-east-03.cleardb.com');
+define('DB_USERNAME', 'b7e42ad3a7a562');
+define('DB_PASSWORD', 'b4bfc92a');
+define('DB_NAME', 'heroku_16e85c529f8feeb');
 
 
 $conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die(mysqli_error());
 $db_select= mysqli_select_db ($conn ,DB_NAME) or die(mysqli_error());
-
-
-
-
-
-
-
-
-?>
